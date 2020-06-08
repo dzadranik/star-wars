c783ed237cdd97e458a1da717d9ae93a570978c0
Victoria <dzadranik@gmail.com>
2020-06-08 20:21:04 +0300

    fixed http url

--- src/api/request.js
+++ src/api/request.js
@@ -1,8 +1,10 @@
 export const loadPersons = (page) =>
     fetch(`https://swapi.dev/api/people/?page=${page}`).then(res => res.json())
 
-export const loadSpecies = (url) =>
-    fetch(url).then(res => res.json())
+export const loadSpecies = (url) => {
+    let replacedUrl = url.replace("http:/", "https:/");
+    return fetch(replacedUrl).then(res => res.json())
+}
 
 export const searchPersons = (value) =>
     fetch(`https://swapi.dev/api/people/?search=${value}`).then(res => res.json())
